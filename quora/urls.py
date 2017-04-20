95626a84b53f19c90529074604e0997ea52b1ec0
Subhadeep <subhadeep.code@gmail.com>
2017-04-21 01:33:06 +0530

    added the answer form and question form and the corresponding functionality.

--- quora/urls.py
+++ quora/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from quora.views import QuestionsList, AnswersList
+from .import views
 
 urlpatterns = [
     url(r'^$', QuestionsList.as_view(), name='questions'),
-    url(r'^(?P<pk>[0-9]+)$', AnswersList.as_view(), name='answers')
+    url(r'^(?P<pk>[0-9]+)$', AnswersList.as_view(), name='answers'),
+    url(r'^ask/$', views.question_view, name='ask'),
+    url(r'^(?P<pk>[0-9]+)/answer/$', views.answer_view, name='answer'),
 
 ]
--- quora/views.py
+++ quora/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, redirect, get_object_or_404
+from django.utils import timezone
 from django.views.generic import ListView, DetailView
+from .forms import QuestionForm, AnswerForm
+
 
 from . models import Question, Answer
 
@@ -12,3 +15,38 @@ class AnswersList(DetailView):
     model = Question
     #context_object_name = 'answers'
     template_name = 'quora/answer_list.html'
+
+def question_view(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.published_date = timezone.now()
+            question.save()
+            return redirect('questions')
+        else:
+            message = 'please fill up the form correctly'
+            form = QuestionForm
+            return render(request, 'quora/ask.html', {'form': form, 'message':message})
+    else:
+        form = QuestionForm(None)
+        return render(request, 'quora/ask.html', {'form':form})
+
+def answer_view(request, pk):
+    if request.method == 'POST':
+        question = get_object_or_404(Question, pk=pk)
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.answered_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('answers', pk=pk)
+        else:
+            message = 'Please check the details'
+            return render(request, 'quora/answer.html', {'form':form})
+    else:
+        form = AnswerForm(None)
+        return render(request, 'quora/answer.html', {'form':form})
